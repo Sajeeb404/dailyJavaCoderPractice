da6a2ad969abcf5ef700d9614d64e270f6b7b1de
Md. Ashraful Islam <ashrafulislam070@gmail.com>
2023-09-14 21:39:57 +0600

    additionQuiz

---
+++ src/main/java/com/mycompany/test/AdditionQuiz.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.test;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Admin
+ */
+public class AdditionQuiz {
+    
+       Scanner input = new Scanner(System.in);  
+    
+    public void additions(int numberOne, int numberTwo){
+        
+        System.out.println("What is " + numberOne + " + " + numberTwo + "?" ); 
+        
+        int answer = input.nextInt();
+        System.out.println("The answer is " + numberOne + " + " + numberTwo + " = " + (numberOne + numberTwo == answer));   
+        
+    
+    } 
+    
+    
+}
--- src/main/java/com/mycompany/test/Test.java
+++ src/main/java/com/mycompany/test/Test.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.test;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Admin
@@ -11,6 +13,15 @@ package com.mycompany.test;
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+     
+         int numbme1 = (int) (System.currentTimeMillis() % 10);
+         int numbme2 = (int) (System.currentTimeMillis() / 7 % 10);
+        AdditionQuiz aq = new AdditionQuiz();
+        
+        aq.additions(numbme1, numbme2);
+        
     }
+    
+    
 }
